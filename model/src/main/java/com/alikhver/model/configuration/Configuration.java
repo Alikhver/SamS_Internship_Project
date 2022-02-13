ea36b14686de6173098f2726e1d4cfec923440f5
Alikhver <dolihver@gmail.com>
2022-02-13 14:40:49 +0300

    Configuration added

--- model/src/main/java/com/alikhver/model/configuration/Configuration.java
+++ model/src/main/java/com/alikhver/model/configuration/Configuration.java
@@ -1,6 +1,29 @@
 package com.alikhver.model.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+
+import javax.persistence.EntityManagerFactory;
+
+@org.springframework.context.annotation.Configuration
+@EnableJpaRepositories(basePackages = "com.alikhver.model")
 public class Configuration {
 
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+        factoryBean.setPersistenceUnitName("TestDB");
 
+        return factoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+
+        return transactionManager;
+    }
 }
