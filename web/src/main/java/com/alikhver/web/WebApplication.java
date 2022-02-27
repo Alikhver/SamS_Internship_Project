84b922d7756b9dc5ef5e98d18454ca72bd68fb64
Alikhver <dolihver@gmail.com>
2022-02-27 15:52:22 +0300

    WebApplication refactored

--- web/src/main/java/com/alikhver/web/WebApplication.java
+++ web/src/main/java/com/alikhver/web/WebApplication.java
@@ -2,10 +2,8 @@ package com.alikhver.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "com.alikhver")
-@EntityScan(basePackages = "com.alikhver.*")
 public class WebApplication {
 
     public static void main(String[] args) {
