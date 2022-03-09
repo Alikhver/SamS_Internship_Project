0fb27d610c320dbcea80995761d369befce3ad0c
Alikhver <dolihver@gmail.com>
2022-03-10 01:28:43 +0300

    DB generation scripts updated

--- model/src/main/resources/db/script/DB generation.sql
+++ model/src/main/resources/db/script/DB generation.sql
@@ -1,158 +1,120 @@
-DROP SCHEMA IF EXISTS `my_db`;
-CREATE SCHEMA `my_db`;
+CREATE SCHEMA IF NOT EXISTS `my_db`;
 USE `my_db`;
 
--- -----------------------------------------------------
--- Table `my_db`.`user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `my_db`.`user`;
 
 CREATE TABLE IF NOT EXISTS `my_db`.`user`
 (
     `id`       BIGINT                             NOT NULL AUTO_INCREMENT,
-    `login`    VARCHAR(45)                        NULL DEFAULT NULL,
-    `password` VARCHAR(45)                        NULL DEFAULT NULL,
-    `role`     ENUM ('USER', 'REDACTOR', 'ADMIN') NULL DEFAULT NULL,
+    `login`    VARCHAR(30)                        ,
+    `password` VARCHAR(30)                        ,
+    `role`     ENUM ('USER', 'REDACTOR', 'ADMIN') ,
     `version`  BIGINT                             DEFAULT 0,
     PRIMARY KEY (`id`)
 );
 
 
-
--- -----------------------------------------------------
--- Table `my_db`.`organisation`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `my_db`.`organisation`;
-
 CREATE TABLE IF NOT EXISTS `my_db`.`organisation`
 (
     `id`           BIGINT      NOT NULL AUTO_INCREMENT,
-    `name`         VARCHAR(45) NULL DEFAULT NULL,
-    `description`  VARCHAR(45) NULL DEFAULT NULL,
-    `is_active`    TINYINT     NULL DEFAULT NULL,
-    `date_created` TIMESTAMP   NULL DEFAULT NULL,
-    `address`      VARCHAR(45) NULL DEFAULT NULL,
+    `name`         VARCHAR(30) ,
+    `description`  VARCHAR(120) ,
+    `is_active`    TINYINT     ,
+    `date_created` TIMESTAMP   ,
+    `address`      VARCHAR(45) ,
     `version`      BIGINT      DEFAULT 0,
     `user_id`      BIGINT      NOT NULL UNIQUE ,
     PRIMARY KEY (`id`, `user_id`),
-    INDEX `fk_organisation_user1_idx` (`user_id` ASC) VISIBLE,
-    CONSTRAINT `fk_organisation_user1`
+    INDEX `fk_organisation_user_idx` (`user_id` ASC),
+    CONSTRAINT `fk_organisation_user`
         FOREIGN KEY (`user_id`)
             REFERENCES `my_db`.`user` (`id`)
 );
 
-
--- -----------------------------------------------------
--- Table `my_db`.`profile`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `my_db`.`profile`;
 
 CREATE TABLE IF NOT EXISTS `my_db`.`profile`
 (
     `id`           BIGINT      NOT NULL AUTO_INCREMENT,
-    `first_name`   VARCHAR(45) NULL DEFAULT NULL,
-    `last_name`    VARCHAR(45) NULL DEFAULT NULL,
-    `phone_number` VARCHAR(45) NULL DEFAULT NULL,
-    `email`        VARCHAR(45) NULL DEFAULT NULL,
-    `date_created` TIMESTAMP   NULL DEFAULT NULL,
+    `first_name`   VARCHAR(45) ,
+    `last_name`    VARCHAR(45) ,
+    `phone_number` VARCHAR(45) ,
+    `email`        VARCHAR(45) ,
+    `date_created` TIMESTAMP   ,
     `version`      BIGINT      DEFAULT 0,
     `user_id`      BIGINT      NOT NULL UNIQUE,
     PRIMARY KEY (`id`, `user_id`),
-    INDEX `fk_profile_user1_idx` (`user_id` ASC) VISIBLE,
-    CONSTRAINT `fk_profile_user1`
+    INDEX `fk_profile_user_idx` (`user_id` ASC),
+    CONSTRAINT `fk_profile_user`
         FOREIGN KEY (`user_id`)
             REFERENCES `my_db`.`user` (`id`)
 );
 
 
--- -----------------------------------------------------
--- Table `my_db`.`utility`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `my_db`.`utility`;
-
 CREATE TABLE IF NOT EXISTS `my_db`.`utility`
 (
     `id`              BIGINT         NOT NULL AUTO_INCREMENT,
-    `name`            VARCHAR(45)    NULL DEFAULT NULL,
-    `price`           DECIMAL(10, 2) NULL DEFAULT NULL,
-    `description`     TEXT           NULL DEFAULT NULL,
+    `name`            VARCHAR(45)    ,
+    `price`           DECIMAL(10, 2) ,
+    `description`     TEXT           ,
     `organisation_id` BIGINT         NOT NULL,
     `version`         BIGINT         DEFAULT 0,
     PRIMARY KEY (`id`, `organisation_id`),
-    INDEX `fk_service_organisation1_idx` (`organisation_id` ASC) VISIBLE,
-    CONSTRAINT `fk_service_organisation1`
+    INDEX `fk_service_organisation_idx` (`organisation_id` ASC),
+    CONSTRAINT `fk_service_organisation`
         FOREIGN KEY (`organisation_id`)
             REFERENCES `my_db`.`organisation` (`id`)
 );
 
-
--- -----------------------------------------------------
--- Table `my_db`.`worker`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `my_db`.`worker`;
 
 CREATE TABLE IF NOT EXISTS `my_db`.`worker`
 (
     `id`              BIGINT      NOT NULL AUTO_INCREMENT,
-    `first_name`      VARCHAR(45) NULL DEFAULT NULL,
-    `last_name`       VARCHAR(45) NULL DEFAULT NULL,
-    `description`     VARCHAR(45) NULL DEFAULT NULL,
+    `first_name`      VARCHAR(45) ,
+    `last_name`       VARCHAR(45) ,
+    `description`     VARCHAR(45) ,
     `organisation_id` BIGINT      NOT NULL,
     `version`         BIGINT      DEFAULT 0,
     PRIMARY KEY (`id`, `organisation_id`),
-    INDEX `fk_worker_organisation1_idx` (`organisation_id` ASC) VISIBLE,
-    CONSTRAINT `fk_worker_organisation1`
+    INDEX `fk_worker_organisation_idx` (`organisation_id` ASC),
+    CONSTRAINT `fk_worker_organisation`
         FOREIGN KEY (`organisation_id`)
             REFERENCES `my_db`.`organisation` (`id`)
 );
 
-
--- -----------------------------------------------------
--- Table `my_db`.`schedule_record`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `my_db`.`schedule_record`;
-
 CREATE TABLE IF NOT EXISTS `my_db`.`schedule_record`
 (
     `id`         BIGINT                                               NOT NULL AUTO_INCREMENT,
-    `time`       TIME                                                 NULL DEFAULT NULL,
-    `status`     ENUM ('BOOKED', 'AVAILABLE', 'DONE', 'CANCELED') NULL DEFAULT NULL,
+    `time`       TIME                                                 ,
+    `status`     ENUM ('BOOKED', 'AVAILABLE', 'DONE', 'CANCELED')     ,
     `profile_id` BIGINT                                               NOT NULL,
     `version`    BIGINT                                               DEFAULT 0,
     `worker_id`  BIGINT                                               NOT NULL,
     `utility_id` BIGINT                                               NOT NULL,
     PRIMARY KEY (`id`, `profile_id`, `worker_id`, `utility_id`),
-    INDEX `fk_schedule_record_profile1_idx` (`profile_id` ASC) VISIBLE,
-    INDEX `fk_schedule_record_worker1_idx` (`worker_id` ASC) VISIBLE,
-    INDEX `fk_schedule_record_service1_idx` (`utility_id` ASC) VISIBLE,
-    CONSTRAINT `fk_schedule_record_profile1`
+    INDEX `fk_schedule_record_profile_idx` (`profile_id` ASC),
+    INDEX `fk_schedule_record_worker_idx` (`worker_id` ASC),
+    INDEX `fk_schedule_record_service_idx` (`utility_id` ASC),
+    CONSTRAINT `fk_schedule_record_profile`
         FOREIGN KEY (`profile_id`)
             REFERENCES `my_db`.`profile` (`id`),
-    CONSTRAINT `fk_schedule_record_service1`
+    CONSTRAINT `fk_schedule_record_service`
         FOREIGN KEY (`utility_id`)
             REFERENCES `my_db`.`utility` (`id`),
-    CONSTRAINT `fk_schedule_record_worker1`
+    CONSTRAINT `fk_schedule_record_worker`
         FOREIGN KEY (`worker_id`)
             REFERENCES `my_db`.`worker` (`id`)
 );
-
-
--- -----------------------------------------------------
--- Table `my_db`.`worker_utility`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `my_db`.`worker_utility`;
 
 CREATE TABLE IF NOT EXISTS `my_db`.`worker_utility`
 (
     `worker_id`  BIGINT NOT NULL,
     `utility_id` BIGINT NOT NULL,
     PRIMARY KEY (`worker_id`, `utility_id`),
-    INDEX `fk_worker_has_utility_utility1_idx` (`utility_id` ASC) VISIBLE,
-    INDEX `fk_worker_has_utility_worker1_idx` (`worker_id` ASC) VISIBLE,
-    CONSTRAINT `fk_worker_has_utility_utility1`
+    INDEX `fk_worker_has_utility_utility_idx` (`utility_id` ASC),
+    INDEX `fk_worker_has_utility_worker_idx` (`worker_id` ASC),
+    CONSTRAINT `fk_worker_has_utility_utility`
         FOREIGN KEY (`utility_id`)
             REFERENCES `my_db`.`utility` (`id`),
-    CONSTRAINT `fk_worker_has_utility_worker1`
+    CONSTRAINT `fk_worker_has_utility_worker`
         FOREIGN KEY (`worker_id`)
             REFERENCES `my_db`.`worker` (`id`)
 );
