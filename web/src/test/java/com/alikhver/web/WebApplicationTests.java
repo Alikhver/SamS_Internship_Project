53890142a6d995509d337d8e8344b0832da2d7ea
Alikhver <dolihver@gmail.com>
2022-03-28 00:33:25 +0300

    tests initial

--- web/src/test/java/com/alikhver/web/WebApplicationTests.java
+++ web/src/test/java/com/alikhver/web/WebApplicationTests.java
@@ -2,7 +2,7 @@ package com.alikhver.web;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest()
 class WebApplicationTests {
 
 //    @Test
---
+++ web/src/test/java/com/alikhver/web/converter/organisation/OrganisationConverterTests.java
@@ -0,0 +1,39 @@
+package com.alikhver.web.converter.organisation;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@Import(OrganisationConverterTests.OrganisationConverterTestsConfig.class)
+public class OrganisationConverterTests {
+
+//    @TestConfiguration()
+//    static class OrganisationConverterTestsConfig {
+//
+//
+//        @Bean
+//        OrganisationToGetOrganisationResponseConverter organisationToGetOrganisationResponseConverter() {
+//            return new OrganisationToGetOrganisationResponseConverter();
+//        }
+//
+//        @Bean
+//        OrganisationConverter organisationConverter() {
+//            return new OrganisationConverter(organisationToGetOrganisationResponseConverter(),
+//
+//                    );
+//        }
+//    }
+//
+////    private final OrganisationConverter organisationConverter;
+//
+//    @Test
+//    public void mapToGetOrganisationResponseTest() {
+//        //Given
+//
+//        //When
+//
+//        //Then
+//    }
+}
