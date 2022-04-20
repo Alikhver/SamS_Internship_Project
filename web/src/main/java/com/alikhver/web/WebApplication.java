78634c58d9511f22c95a05ef1f0657801c4a0c7f
Alikhver <dolihver@gmail.com>
2022-04-21 02:15:58 +0300

    Error 404 & 500 pages added

--- web/src/main/java/com/alikhver/web/WebApplication.java
+++ web/src/main/java/com/alikhver/web/WebApplication.java
@@ -2,9 +2,10 @@ package com.alikhver.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = "com.alikhver")
+@SpringBootApplication(scanBasePackages = "com.alikhver", exclude = {ErrorMvcAutoConfiguration.class})
 @EnableScheduling
 public class WebApplication {
 
